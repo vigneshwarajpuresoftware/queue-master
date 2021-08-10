6800626573b7cd1edd28f5489b321b147748f66a
A J Vigneshwar <86348043+vigneshwarajpuresoftware@users.noreply.github.com>
2021-08-10 08:41:22 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,5 +27,8 @@ cp $CODE_DIR/target/*.jar $CODE_DIR/docker/$(basename $CODE_DIR)
 
 for m in ./docker/*/; do
     REPO=${GROUP}/$(basename $m)
+    echo $REPO
     $DOCKER_CMD build -t ${REPO}:${COMMIT} $CODE_DIR/$m;
+    echo $CODE_DIR
+    echo $COMMIT
 done;
